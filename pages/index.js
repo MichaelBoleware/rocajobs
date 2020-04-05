792754f14d97cd65ce5dd04a88f603a1b5c45ad3
MichaelBoleware <61945650+MichaelBoleware@users.noreply.github.com>
2020-04-05 14:00:55 -0500

    Update index.js
    added link to registration

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,10 @@ export default function Index() {
         <Link href="/login">
           <a>login</a>
         </Link>
+      <p>Don't have an account?</p>
+      <Link href="/register">
+        <a>Click here!</a>
+      </Link>
         <p>index page (to be removed)</p>
       </div>
     );
