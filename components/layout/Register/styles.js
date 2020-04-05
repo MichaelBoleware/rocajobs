57f37fbce3789818649992caa5fb9a01037f72ed
MichaelBoleware <61945650+MichaelBoleware@users.noreply.github.com>
2020-04-05 11:15:23 -0500

    Create styles.js

---
+++ components/layout/Register/styles.js
@@ -0,0 +1,12 @@
+import { css } from "@emotion/core";
+
+export const registerCLs = css`
+        background: 'transparent'
+        color: '#999'
+        border: 0
+        borderBottom: '1px solid #666'
+        borderRadius: 0
+        fontSize: '3rem'
+        fontWeight: 500
+        boxShadow: 'none !important'
+        `;
